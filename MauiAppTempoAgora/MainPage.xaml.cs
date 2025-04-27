c56c4b45f3f299eec7f2801f1bb3c69d08f50cf8
Tiemi <jellyuura@gmail.com>
2025-04-27 14:27:35 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -0,0 +1,138 @@
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+using System;
+using System.Diagnostics;
+
+namespace MauiAppTempoAgora
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked_Previsao(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
+
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Descrição do clima: {t.description} \n" +
+                                         $"Temp Máx: {t.temp_max}°C \n" +
+                                         $"Temp Min: {t.temp_min}°C \n" +
+                                         $"Velocidade do vento: {t.speed} m/s \n" +
+                                         $"Visibilidade: {t.visibility} metros \n" +
+                                         $"Nascer do Sol: {FormatarHora(t.sunrise)} \n" +
+                                         $"Pôr do Sol: {FormatarHora(t.sunset)}";
+
+                        lbl_res.Text = dados_previsao;
+
+                        string mapa = $"https://embed.windy.com/embed.html?" +
+                                      $"type=map&location=coordinates&metricRain=mm&metricTemp=°C" +
+                                      $"&metricWind=km/h&zoom=5&overlay=wind&product=ecmwf&level=surface" +
+                                      $"&lat={t.lat.ToString().Replace(",", ".")}&lon={t.lon.ToString().Replace(",", ".")}";
+
+                        wv_mapa.Source = mapa;
+
+                        Debug.WriteLine(mapa);
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Sem dados de Previsão.";
+                    }
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private async void Button_Clicked_Localizacao(object sender, EventArgs e)
+        {
+            try
+            {
+                GeolocationRequest request = new GeolocationRequest(
+                    GeolocationAccuracy.Medium,
+                    TimeSpan.FromSeconds(10)
+                );
+
+                Location? local = await Geolocation.Default.GetLocationAsync(request);
+
+                if (local != null)
+                {
+                    string local_disp = $"Latitude: {local.Latitude} \n" +
+                                        $"Longitude: {local.Longitude}";
+
+                    lbl_coords.Text = local_disp;
+
+                    GetCidade(local.Latitude, local.Longitude);
+                }
+                else
+                {
+                    lbl_coords.Text = "Nenhuma localização encontrada.";
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Erro: Dispositivo não Suporta", fnsEx.Message, "OK");
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                await DisplayAlert("Erro: Localização Desabilitada", fneEx.Message, "OK");
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Erro: Permissão da Localização", pEx.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+        private async void GetCidade(double lat, double lon)
+        {
+            try
+            {
+                IEnumerable<Placemark> places = await Geocoding.Default.GetPlacemarksAsync(lat, lon);
+
+                Placemark? place = places.FirstOrDefault();
+
+                if (place != null)
+                {
+                    txt_cidade.Text = place.Locality;
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro: Obtenção do nome da Cidade", ex.Message, "OK");
+            }
+        }
+
+        private string FormatarHora(string horario)
+        {
+            if (DateTime.TryParse(horario, out DateTime dataHora))
+            {
+                return dataHora.ToString("HH:mm");
+            }
+            else
+            {
+                return horario;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,47 @@
+Repositório do projeto de Consumo de Web Services usando .NET MAUI
+
+## Playlist com todas as videoaulas de construção desse projeto
+- https://www.youtube.com/watch?v=ExstAh2vqjk&list=PLHVpcBDJr5dkC1RVRMbVsgpZgtfOxMtf-
+
+## Conteúdo das Videoaulas
+### VIDEOAULA 1: CRIANDO APLICATIVOS COM .NET MAUI - CONSUMINDO WEB SERVICES - PARTE I (1h04mim):
+- 00:00 Introdução
+- 01:11 Criação do novo projeto
+- 04:20 Adicionando o projeto ao GitHub
+- 05:20 Compilando o projeto
+- 06:30 Apresentação da API Open Weather
+- 10:09 Retorno da API e JSON
+- 15:25 Instalando o pacote NewtonsoftJSON
+- 18:22 Criando a estrutura do Projeto
+- 21:00 Criando Model a partir do JSON
+- 26:55 Implementando o consumo do WebService
+- 30:33 Definindo a chave da API
+- 31:08 Definindo a URL da API
+- 33:01 Requisições HTTP com HttpClient
+- 33:26 Laço using e requisições HTTP
+- 34:26 Obtendo a resposta HTTP da API
+- 36:07 Processando a resposta da API
+- 37:31 Fazendo o parse da String recebida para JSON
+- 38:56 Recomendo using não usados
+- 39:50 Preenchendo o Model com dados do JSON
+- 48:26 Processando as datas com DateTime
+- 53:53 Implementando a interface gráfica
+- 55:46 Chamando a DataService para consumir o WebService
+
+### VIDEOAULA 2: CRIANDO APLICATIVOS COM .NET MAUI - CONSUMINDO WEB SERVICES - PARTE II (58mim):
+- 00:00 Introdução
+- 02:22 Obtendo a localização do dispositivo
+- 06:26 Diferenciando os tipos de exceções
+- 10:56 Implementando a localização com Geolocation
+- 17:00 Mostrando as informações de Geolocation
+- 21:00 Ajustando as permissões de localização do Windows
+- 22:47 Obtendo as informações de localização
+- 24:00 Obtendo o nome da localização do dispositivo 
+- 26:38 Obtendo o Placemark
+- 33:20 Sobre o Token de Mapas no Windows
+- 35:20 Definindo o Token do BingMaps no Windows
+- 38:00 Adicionando o mapa climático da Windy
+- 43:00 Definindo uma WebView para mostrar do mapa
+
+## CURTIU MEU TRABALHO?
+Você pode me ajudar com um pix para tiago@tiago.blog.br 🍻
